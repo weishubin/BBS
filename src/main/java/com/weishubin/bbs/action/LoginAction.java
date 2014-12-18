730912343d3f8e407580f1c355ca7e97c4a3d1fc
wei_shubin <wei_shubin@gmail.com>
2014-12-18 17:27:32 +0800

    config struts

---
+++ src/main/java/com/weishubin/bbs/action/LoginAction.java
@@ -0,0 +1,20 @@
+package com.weishubin.bbs.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class LoginAction extends ActionSupport {
+	
+	public String welcome() throws Exception {
+        
+	    return SUCCESS;
+	 
+	}
+	
+	public String execute() throws Exception {
+	         
+	    return SUCCESS;
+	 
+	}
+	 
+}
