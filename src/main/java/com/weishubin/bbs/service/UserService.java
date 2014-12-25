59e09135b68b5434de82314b127ce88e1814494f
wei_shubin <wei_shubin@gmail.com>
2014-12-25 13:52:36 +0800

    add MD5

--- src/main/java/com/weishubin/bbs/service/UserService.java
+++ src/main/java/com/weishubin/bbs/service/UserService.java
@@ -2,33 +2,34 @@ package com.weishubin.bbs.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.weishubin.bbs.dao.UserMapper;
 import com.weishubin.bbs.domain.User;
+import com.weishubin.bbs.util.MD5Util;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserMapper userDao;
-	
+
 	public UserMapper getUserDao() {
 		return userDao;
 	}
 
-	
 	public void setUserDao(UserMapper userDao) {
 		this.userDao = userDao;
 	}
 
-	@Transactional
 	public User login(String usrNo, String pwd) {
 		User user = userDao.getUser(usrNo);
 		if (user == null) {
 			return null;
 		} else {
-			return user;
+			if (user.getPwd().equalsIgnoreCase(MD5Util.MD5(pwd))) {
+				return user;
+			} else
+				return null;
 		}
 	}
 
